353ad17d636e6085797b579bf0f1e4541890c211
zaharapple <zaharapple88@gmail.com>
2021-03-19 13:59:09 +0200

    first vesion

---
+++ app.py
@@ -0,0 +1,57 @@
+from models import Employee, Recruiter, Programmer, Candidate, Vacancy
+
+
+def main():
+    r = Recruiter("Sasha", 150, 21)
+    p = Programmer("Slava", 220, 23, {'have a car ', 'Python', 'JS', 'Git', 'HTML'})
+    p2 = Programmer("Kostya", 200, 24, {'know of English ', 'Python', 'Django'})
+    c = Candidate("Petya", "pet@com", "Razrabotka", "JS", "trenie")
+    c2 = Candidate("Katya", "KET@com", "Recruter", "English", "С1")
+    c3 = Candidate("Kiril", "Kir@com", "Razrabotka", "Python", "junior")
+    v = Vacancy("Programmer", "Python,JS,HTML", "junior")
+    v2 = Vacancy("Recruiter", "комуникабельность , знание английского языка", "стаж 1год")
+
+    print(v)
+    print(v2)
+
+    print('---------------------------')
+
+    print(c)
+    print(c2)
+    print(c3)
+
+    print('---------------------------')
+
+    print(r)
+    print(r.work())
+    print(r.cheak_salary())
+
+    print('---------------------------')
+
+    print(p)
+    print(p.work())
+    print(p.cheak_salary())
+    print(p.tech_stack)
+
+    print('---------------------------')
+    print(r == p)  # comparison by salary
+    print()
+
+    print(p2)
+    print(p2.work())
+    print(p2.cheak_salary())
+    print(p2.tech_stack)
+
+    print('---------------------------')
+    print(p > p2)
+    print()
+
+    alfaprogr = p + p2
+    print(alfaprogr)
+    print(alfaprogr.work())
+    print(alfaprogr.cheak_salary())
+    print(alfaprogr.tech_stack)
+
+
+if __name__ == '__main__':
+    main()
---
+++ models.py
@@ -0,0 +1,125 @@
+import datetime
+import math
+
+
+class Employee:
+    def __init__(self, name, zp_day, days):
+        self.name = name
+        self.zp_day = zp_day
+        self.days = days
+
+    def work(self):
+        return "I come to the office"
+
+    def cheak_salary(self):
+        def zp(zp_day):
+            first_data = datetime.date.today() - datetime.timedelta(days=datetime.date.today().day)
+            data_now = datetime.date.today()
+            delta = datetime.timedelta(1)
+
+            days = 0
+            while first_data != data_now:
+                if data_now.isoweekday() not in (6, 7):
+                    days += 1
+                data_now -= delta
+            return zp_day * days
+
+        zp_now = zp(self.zp_day)
+        zp_month = Employee.zp_month_calc(self)
+        return f"Зарплата за месяц: {zp_month}. Зарплата на текущий рабочий день: {zp_now}"
+
+    def zp_month_calc(self):
+        return self.zp_day * self.days
+
+    def __gt__(self, other):
+        return self.zp_month_calc() > other.zp_month_calc()
+
+    def __ge__(self, other):
+        return self.zp_month_calc() >= other.zp_month_calc()
+
+    def __lt__(self, other):
+        return self.zp_month_calc() < other.zp_month_calc()
+
+    def __le__(self, other):
+        return self.zp_month_calc() <= other.zp_month_calc()
+
+    def __eq__(self, other):
+        return self.zp_month_calc() == other.zp_month_calc()
+
+    def __ne__(self, other):
+        return self.cheak_salary() != other.cheak_salary()
+
+
+class Recruiter(Employee):
+
+    def work(self):
+        return 'I come to the office and start to hiring.'
+
+    def __str__(self):
+        return f"{self.__class__.__name__} : {self.name}"
+
+
+class Programmer(Employee):
+
+    def __init__(self, name, zp_day, days, tech_stack):
+        super().__init__(name, zp_day, days)
+        self.tech_stack = tech_stack
+
+    def work(self):
+        return 'I come to the office and start to coding.'
+
+    def __str__(self):
+        return f"{self.__class__.__name__} : {self.name}"
+
+    def __gt__(self, other):
+        return len(self.tech_stack) > len(other.tech_stack)
+
+    def __ge__(self, other):
+        return len(self.tech_stack) >= len(other.tech_stack)
+
+    def __lt__(self, other):
+        return len(self.tech_stack) < len(other.tech_stack)
+
+    def __le__(self, other):
+        return len(self.tech_stack) <= len(other.tech_stack)
+
+    def __eq__(self, other):
+        return len(self.tech_stack) == len(other.tech_stack)
+
+    def __ne__(self, other):
+        return len(self.tech_stack) != len(other.tech_stack)
+
+    def __add__(self, other):
+        tech_stack = self.tech_stack | other.tech_stack
+        name = self.name + other.name
+        zp_day = self.zp_day + other.zp_day
+        day = math.ceil((self.days + other.days) / 2)
+        return Programmer(name, zp_day, day, tech_stack)
+
+
+class Candidate:
+    def __init__(self, full_name, email, technologies, main_skill, main_skill_grade):
+        self.full_name = full_name
+        self.email = email
+        self.technologies = technologies
+        self.main_skill = main_skill
+        self.main_skill_grade = main_skill_grade
+
+    def __str__(self):
+        return f"""{self.__class__.__name__} : {self.full_name} 
+            адресс: {self.email} 
+            на должность: {self.technologies} , main_skill: {self.main_skill} на уровне {self.main_skill_grade}       
+        """
+
+
+class Vacancy:
+    def __init__(self, title, main_skill, main_skill_level):
+        self.title = title
+        self.main_skill = main_skill
+        self.main_skill_level = main_skill_level
+
+    def __str__(self):
+        return f"""{self.__class__.__name__} : {self.title} 
+            Главное знание: {self.main_skill} 
+            Уровень: {self.main_skill_level}       
+        """
